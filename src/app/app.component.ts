00e6922538009dc53ba3806404435f3e9250a00a
ayblue004 <buluroayomide@gmail.com>
2024-08-20 21:53:47 +0100

    Background files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'folio';
+  bgNum: number = 1;
+  bgLink: string = 'bg' + this.bgNum.toString()
+
+  ngOnInit(): void {
+    setInterval(()=>{
+      if(this.bgNum < 8){
+        this.bgNum = this.bgNum + 1
+        this.bgLink = 'bg' + this.bgNum.toString()
+      }else{
+        this.bgNum = 1
+        this.bgLink = 'bg' + this.bgNum.toString()
+      }
+    }, 5000)
+  }
+
 }
